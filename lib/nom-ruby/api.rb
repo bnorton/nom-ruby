36dea95b1a835646b13797e347210b3e7e0618b4
Brian Norton <brian.nort@gmail.com>
2011-12-30 01:38:02 -0800

    Locations and Users (basic is there now)

--- lib/nom-ruby/api.rb
+++ lib/nom-ruby/api.rb
@@ -2,10 +2,9 @@ module Nom
   class API
     
     def here(lat, lng, options = {})
-      puts "HERE #{lat} - #{lng} - #{options.inspect}"
       start, limit = Nom::Util.page(options)
       Nom::API.handle.get('/locations/here', {
-        :lat => lng,
+        :lat => lat,
         :lng => lng,
         :dist => (options[:dist] || 0.5),
         :start => start,
@@ -13,10 +12,57 @@ module Nom
       })
     end
 
-    # keep this around for performance reasons
+    def location_search(query, options = {})
+      start, limit = Nom::Util.page(options)
+      params = { :query => query }
+
+      params.merge!({
+        :lat => options[:lat],
+        :lng => options[:lng]
+      }) if(options[:lat].present? && options[:lng].present?)
+
+      params.merge!({
+        :location => options[:location]
+      }) if options[:location].present?
+
+      Nom::API.handle.get('/locations/search', params)
+    end
+
+    def register(email, password, options ={})
+      Nom::API.handle.post('/users/register', {
+        :email => email,
+        :password => password,
+        :screen_name => options[:screen_name],
+        :name => options[:name],
+        :city => options[:city]
+      })
+    end
+
+    # Any item out of email, user_nid, screen_name will work
+    def login(password, options ={})
+      Nom::API.handle.post('/users/login', {
+        :password => password,
+        :email => options[:email],
+        :user_nid => options[:user_nid],
+        :screen_name => options[:screen_name]
+      })
+    end
+
+    def user_search(query, options = {})
+      start, limit = Nom::Util.page(options)
+      Nom::API.handle.get('/locations/search', {
+        :query => query, # If the input value is of unknown origin
+        :screen_name => options[:screen_name],
+        :email => options[:email]
+      })
+    end
+
+    private
+    
+    # keep this around for re-use
     def self.handle
       @handle ||= Nom::Fetcher.new # (:key => Nom::Keys.developer, :secret => Nom::Keys.secret)
     end
-    
+
   end
 end
--- lib/nom-ruby/version.rb
+++ lib/nom-ruby/version.rb
@@ -1,10 +1,9 @@
 module GemNom
   module VERSION
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 8
-    PRE   = nil
-
+    MAJOR  = 0
+    MINOR  = 2
+    PATCH  = 0
+    PRE    = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 end
