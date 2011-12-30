e1f663dc48b1bfddd2091d1a7c67dbbc44a6c386
Brian Norton <brian.nort@gmail.com>
2011-12-30 01:37:39 -0800

    require the right file for api

--- lib/nom-ruby.rb
+++ lib/nom-ruby.rb
@@ -4,7 +4,8 @@ require 'net/https'
 require 'active_support'
 require 'json'
 
-require 'nom-ruby/api'
 require 'nom-ruby/fetcher'
 require 'nom-ruby/keys'
 require 'nom-ruby/util'
+
+require 'nom-ruby/api'
